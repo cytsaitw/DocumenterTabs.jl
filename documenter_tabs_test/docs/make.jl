3aa19cad7aeb5600b5865d1101295d32323da5e5
cytsaitw <21618joy@gmail.com>
2026-02-20 14:22:48 +0800

    Enable preview deployments in make.jl

--- documenter_tabs_test/docs/make.jl
+++ documenter_tabs_test/docs/make.jl
@@ -2,11 +2,16 @@ using Documenter
 using DocumenterTabs          # 1. import the package
 
 makedocs(
-    sitename = "Test",
-    plugins  = [DocumenterTabs.DocumenterTabsPlugin()],   # 2. register the plugin
-    pages    = [
-        "Home"     => "index.md",
+    sitename="Test",
+    plugins=[DocumenterTabs.DocumenterTabsPlugin()],   # 2. register the plugin
+    pages=[
+        "Home" => "index.md",
         "Tutorial" => "tutorial.md",
     ],
+)
+
+deploydocs(
+    repo="github.com/cytsaitw/DocumenterTabs.jl.git",
+    push_preview=true,
 )
 
